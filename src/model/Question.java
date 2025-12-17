2126b6c7ef7f942c8d85c20088fe3bdb0b9d6a9e
swap1l <kilian.bayer@gmx.at>
2025-12-17 15:21:06 +0100

    Logik für die Question Hierachie erstellt.

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,4 +1,22 @@
 package model;
 
 public abstract class Question {
+    protected String questionText;
+    protected String correctAnswer;
+
+    public Question(String questionText, String correctAnswer) {
+        this.correctAnswer = questionText;
+        this.questionText = questionText;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public abstract boolean checkAnswer(String answer);
+
 }
--- src/model/TextQuestion.java
+++ src/model/TextQuestion.java
@@ -1,5 +1,17 @@
 package model;
 
-public class TextQuestion {
+public class TextQuestion extends Question {
+
+    public TextQuestion(String questionText, String answerText) {
+        super(questionText, answerText);
+    }
+    @Override
+    public boolean checkAnswer(String answer) {
+        if(answer == null) {
+            return false;
+        }
+        return answer.trim().equals(getCorrectAnswer().trim());
+    }
+
 
 }
